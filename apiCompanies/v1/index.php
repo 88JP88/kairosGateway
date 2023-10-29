b463dc618186b23367b89269769a417b938fa1f7
Juan <juan.paez@api-ux.com>
2023-10-28 19:15:43 -0500

    pr base de prueba

--- apiCompanies/v1/index.php
+++ apiCompanies/v1/index.php
@@ -673,7 +673,7 @@ Flight::route('GET /getCalendarDays/@headerslink/@filter/@param', function ($hea
 
 
 
-Flight::route('GET /getCalendarTimedes/@headerslink/@filter/@param', function ($headerslink,$filter,$param) {
+Flight::route('GET /getCalendarTimedes/@headerslink/@filter/', function ($headerslink,$filter) {
     
     header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
@@ -736,7 +736,7 @@ Flight::route('GET /getCalendarTimedes/@headerslink/@filter/@param', function ($
             $context = stream_context_create($options);
             
             // Realizar la solicitud y obtener la respuesta
-            $response = file_get_contents($sub_domain.'/kairosCore/apiCompanies/v1/getCalendarTimedes/'.$filter.'/'.$param, false, $context);
+            $response = file_get_contents($sub_domain.'/kairosCore/apiCompanies/v1/getCalendarTimedes/'.$filter, false, $context);
                  
            
         
