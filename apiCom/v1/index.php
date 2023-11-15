e41ad47f028ada7c3cd8aab65846e7c53266ee10
Juan <juan.paez@api-ux.com>
2023-11-15 15:11:53 -0500

    pr base de prueba

--- apiCom/v1/index.php
+++ apiCom/v1/index.php
@@ -281,5 +281,87 @@ echo $response2;
 });
 
 
+Flight::route('POST /postCategorie/@apk/@xapk', function ($apk,$xapk) {
+  
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+    header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+    
+   
+    // Verificar si los encabezados 'Api-Key' y 'Secret-Key' existen
+    if (!empty($apk) && !empty($xapk)) {
+        $dta = array(
+            
+            'clientId' => Flight::request()->data->clientId,
+            'categoryName' => Flight::request()->data->categoryName,
+            'comments' => Flight::request()->data->comments,
+            'parentId' => Flight::request()->data->parentId,
+            'categoryType' => Flight::request()->data->categoryType
+
+        );
+
+
+
+        // Acceder a los encabezados
+    
+        
+
+        $sub_domaincon=new model_dom();
+        $sub_domain=$sub_domaincon->dom();
+        $url = $sub_domain.'/kairosCore/apiAuth/v1/authApiKeyGateway/';
+      
+        $data = array(
+          'ApiKey' =>$apk, 
+          'xapiKey' => $xapk
+          
+          );
+      $curl = curl_init();
+      
+      // Configurar las opciones de la sesión cURL
+      curl_setopt($curl, CURLOPT_URL, $url);
+      curl_setopt($curl, CURLOPT_POST, true);
+      curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+      curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+      // curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+      
+      // Ejecutar la solicitud y obtener la respuesta
+      $response1 = curl_exec($curl);
+
+      
+      $dt=json_encode($dta);
+
+      curl_close($curl);
+      $sub_domain1=$sub_domaincon->domCom();
+      $url = $sub_domain1."/kairosCom/apiCom/v1/postCategorie/$apk/$xapk";
+
+      $curl = curl_init();
+      
+      // Configurar las opciones de la sesión cURL
+      curl_setopt($curl, CURLOPT_URL, $url);
+      curl_setopt($curl, CURLOPT_POST, true);
+      curl_setopt($curl, CURLOPT_POSTFIELDS, $dt);
+      curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+      $headers = array(
+        'Content-Type: application/json'
+    );
+    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+      
+      // Ejecutar la solicitud y obtener la respuesta
+      $response2 = curl_exec($curl);
+      
+
+ 
+    curl_close($curl);
+
+echo $response2;
+
+        
+    } else {
+        echo 'false|¡Error: Encabezados faltantes!';
+    }
+});
+
+
 
 Flight::start();
