3ad2fd772ab6a909755168faef99b889a86411b0
Juan <juan.paez@api-ux.com>
2023-12-12 21:17:56 -0500

    pr base de prueba

--- apiClient/v1/index.php
+++ apiClient/v1/index.php
@@ -2095,7 +2095,7 @@ Flight::route('GET /getClientOrders/@headerslink/@clientId/@filter/@param/@value
             $context = stream_context_create($options);
             
             // Realizar la solicitud y obtener la respuesta
-            $response = file_get_contents($sub_domain.'/kairosCom/apiClient/v1/getClientOrders/'.$clientId.'/'.$filter.'/'.$param.'/'.$value, false, $context);
+            //$response = file_get_contents($sub_domain.'/kairosCom/apiClient/v1/getClientOrders/'.$clientId.'/'.$filter.'/'.$param.'/'.$value, false, $context);
                  
            
         
