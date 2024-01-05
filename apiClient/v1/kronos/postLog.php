7b4d1fae49bc8ded3676f1ed110c35146f5aa4c5
Juan <juan.paez@api-ux.com>
2024-01-05 18:14:44 -0500

    pr base de prueba

--- apiClient/v1/kronos/postLog.php
+++ apiClient/v1/kronos/postLog.php
@@ -71,7 +71,7 @@ $jsonData = '{
   // Configurar las opciones de la sesión cURL
   curl_setopt($curl, CURLOPT_URL, $url);
   curl_setopt($curl, CURLOPT_POST, true);
-  curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+  curl_setopt($curl, CURLOPT_POSTFIELDS, $json_data);
   curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
   
   // Ejecutar la solicitud y obtener la respuesta
