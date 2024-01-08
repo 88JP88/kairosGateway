9265b89ee95f76130c01fd5e264f3df297a8c491
Juan <juan.paez@api-ux.com>
2024-01-08 13:15:16 -0500

    pr base de prueba

--- apiCom/v1/kronos/postLog.php
+++ apiCom/v1/kronos/postLog.php
@@ -1,6 +1,6 @@
 <?php
 
-function kronos($response,$message,$error,$function,$filename,$module,$clientId,$data,$endpoint,$statusCode) {
+function kronos($valores) {
 
   // Establecer la zona horaria a Bogotá
 date_default_timezone_set('America/Bogota');
@@ -11,52 +11,26 @@ $now->setTimezone(new DateTimeZone('America/Bogota'));
 
 // Formatear la fecha y hora actual
 $currentDateTime = $now->format('Y-m-d H:i:s');
-if($response==="true"){
-    $level="info";
-}
-if($response==="error"){
-    $level="error";
-}
-$ip = $_SERVER['REMOTE_ADDR'];
-$details = json_decode(file_get_contents("http://ip-api.com/json/$ip"));
+
 
 $jsonData = '{
     "log":{
       "front":{
-        "timestamp": "'.$currentDateTime.'",
-        "level": "'.$level.'",
-        "clientId": "'.$clientId.'",
-        "module": "'.$module.'",
-        "domain":"'.$_SERVER['HTTP_HOST'].'",
-        "function":"'.$function.'",
-        "file":"'.$filename.'",
-        "response":"'.$response.'",
-        "error":"'.$error.'",
-        "clientIp":"'.$_SERVER['REMOTE_ADDR'].'",
-        "clientBrowser":"'.$_SERVER['HTTP_USER_AGENT'].'",
-        "clientLocation":"'.$details->country.' / '.$details->city.'"
-      },
-      "infoLog":{
-        "endPoint":"'.$endpoint.'",
-        
-        "response":"'.$response.'",
-        "message":"'.$message.'"
+        "timestamp": "'.$valores.'"
       }
-    },    "status":{
-      "code":"'.$statusCode.'"
-    }
+      
   }';
 
 
 
 
   
-  $url ="https://dev-kronos.lugma.tech/kronos/apiLogs/v1/middleLog/";
+  $url ="https://dev-kronos.lugma.tech/kronos/apiLogs/v1/prueba/";
   
   // Definir los datos a enviar en la solicitud POST
   $data6 = array(
-      'data' => $jsonData,
-      'logType' => 'gatewayApiCom'
+      'data' => $jsonData
+     
       
   );
   
@@ -69,7 +43,7 @@ $jsonData = '{
   // Configurar las opciones de la sesión cURL
   curl_setopt($curl, CURLOPT_URL, $url);
   curl_setopt($curl, CURLOPT_POST, true);
-  curl_setopt($curl, CURLOPT_POSTFIELDS, $json_data);
+  curl_setopt($curl, CURLOPT_POSTFIELDS, $data6);
   curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
   
   // Ejecutar la solicitud y obtener la respuesta
