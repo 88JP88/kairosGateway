6f4f9e45d9479e79e4cad6f227545782b7ff79b0
Juan <juan.paez@api-ux.com>
2024-01-08 13:20:14 -0500

    pr base de prueba

--- apiCom/v1/kronos/postLog.php
+++ apiCom/v1/kronos/postLog.php
@@ -22,12 +22,9 @@ $jsonData = '{
         "level": "info",
         "clientId": "'.$clientId.'",
         "module": "'.$module.'",
-        "domain":"'.$_SERVER['HTTP_HOST'].'",
         "function":"'.$function.'",
         "file":"'.$filename.'",
         "error":"'.$error.'",
-        "clientIp":"'.$_SERVER['REMOTE_ADDR'].'",
-        "clientBrowser":"'.$_SERVER['HTTP_USER_AGENT'].'",
         "clientLocation":"'.$details->country.' / '.$details->city.'"
       },
       "infoLog":{
