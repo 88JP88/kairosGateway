9595f8dce17a3e7cb1ba485aa690441135f71ed8
Juan <juan.paez@api-ux.com>
2024-01-10 14:59:19 -0500

    pr base de prueba

--- apiCore/v1/index.php
+++ apiCore/v1/index.php
@@ -1358,23 +1358,7 @@ Flight::route('POST /validateLogIn/@headerslink', function ($headerslink) {
         // Realizar acciones basadas en los valores de los encabezados
   //echo "true";
 
-     //inicio de log
-     require_once 'kronos/postLog.php';
- 
-     $backtrace = debug_backtrace();
-     $info['Función'] = $backtrace[1]['function']; // 1 para obtener la función actual, 2 para la anterior, etc.
-     $currentFile = __FILE__; // Obtiene la ruta completa y el nombre del archivo actual
-    $justFileName = basename($currentFile);
-    $rutaCompleta = __DIR__;
-    $status = http_response_code();
-    $cid=Flight::request()->data->clientId;
-    
-    //$response1 = trim($response1); // Eliminar espacios en blanco alrededor de la respuesta
-    $array = explode("|", $response2);
-    $response12=$array[0];
-    $message=$array[1];
-    kronos($response12,$message,$message, $info['Función'],$justFileName,$rutaCompleta,$cid,$dt,$url,$status);
-    //final de log
+   
 echo $response2;
 //echo json_encode($dta);
         
