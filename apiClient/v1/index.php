29b10931eb66dc7e5b04c9f9145ced10d799a89f
Juan <juan.paez@api-ux.com>
2024-01-21 11:19:58 -0500

    pr base de prueba

--- apiClient/v1/index.php
+++ apiClient/v1/index.php
@@ -626,11 +626,11 @@ Flight::route('POST /postClientOrder/@apk/@xapk', function ($apk,$xapk) {
      $cid=Flight::request()->data->clientId;
      
      //$response1 = trim($response1); // Eliminar espacios en blanco alrededor de la respuesta
-     $array = explode("|", $response2);
-     $response12=$array[0];
-     $message=$array[1];
-     kronos($response12,$message,$message, $info['Función'],$justFileName,$rutaCompleta,$cid,$dt,$url,$status);
-     //final de log
+    //  $array = explode("|", $response2);
+    //  $response12=$array[0];
+    //  $message=$array[1];
+    //  kronos($response12,$message,$message, $info['Función'],$justFileName,$rutaCompleta,$cid,$dt,$url,$status);
+    //  //final de log
 
 echo $response2;
 
