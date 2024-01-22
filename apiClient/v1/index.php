d1d686593ccee770133f16780ecb7f847d2a94ed
Juan <juan.paez@api-ux.com>
2024-01-22 12:31:36 -0500

    pr base de prueba

--- apiClient/v1/index.php
+++ apiClient/v1/index.php
@@ -3,8 +3,9 @@
 
 require 'flight/Flight.php';
 
-require 'database/db_users.php';
 require 'env/domain.php';
+require 'model/users/modelPost.php';
+require 'model/modelSecurity/authModel.php';
 
  
 
@@ -34,85 +35,22 @@ Flight::route('POST /postProduct/@apk/@xapk', function ($apk,$xapk) {
             'inPrice' => Flight::request()->data->inPrice,
             'providerId' => Flight::request()->data->providerId,
             'imgUrl' => Flight::request()->data->imgUrl,
-            'techSpef' => Flight::request()->data->techSpef
+            'techSpef' => Flight::request()->data->techSpef,
+            'apk' => $apk,
+            'xapk' => $xapk
 
         );
 
 
-
+            authModel::modelAuth($apk,$xapk);//AUTH MODEL
         // Acceder a los encabezados
     
         
 
-        $sub_domaincon=new model_dom();
-        $sub_domain=$sub_domaincon->dom();
-        $url = $sub_domain.'/kairosCore/apiAuth/v1/authApiKeyGateway/';
-      
-        $data = array(
-          'ApiKey' =>$apk, 
-          'xapiKey' => $xapk
-          
-          );
-      $curl = curl_init();
-      
-      // Configurar las opciones de la sesión cURL
-      curl_setopt($curl, CURLOPT_URL, $url);
-      curl_setopt($curl, CURLOPT_POST, true);
-      curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-      curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-      // curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-      
-      // Ejecutar la solicitud y obtener la respuesta
-      $response1 = curl_exec($curl);
+       
 
-      
-      $dt=json_encode($dta);
-
-      curl_close($curl);
-      $sub_domain1=$sub_domaincon->domCom();
-      $url = $sub_domain1."/kairosCom/apiClient/v1/postProduct/$apk/$xapk";
-
-      $curl = curl_init();
-      
-      // Configurar las opciones de la sesión cURL
-      curl_setopt($curl, CURLOPT_URL, $url);
-      curl_setopt($curl, CURLOPT_POST, true);
-      curl_setopt($curl, CURLOPT_POSTFIELDS, $dt);
-      curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-
-      $headers = array(
-        'Content-Type: application/json'
-    );
-    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-      
-      // Ejecutar la solicitud y obtener la respuesta
-      $response2 = curl_exec($curl);
-      
-
- 
-    curl_close($curl);
-
-
+     echo modelPost::postProduct($dta);
     
-      //inicio de log
-      require_once 'kronos/postLog.php';
- 
-      $backtrace = debug_backtrace();
-      $info['Función'] = $backtrace[1]['function']; // 1 para obtener la función actual, 2 para la anterior, etc.
-      $currentFile = __FILE__; // Obtiene la ruta completa y el nombre del archivo actual
-     $justFileName = basename($currentFile);
-     $rutaCompleta = __DIR__;
-     $status = http_response_code();
-     $cid=Flight::request()->data->clientId;
-     $trackId=Flight::request()->data->trackId;
-     
-     //$response1 = trim($response1); // Eliminar espacios en blanco alrededor de la respuesta
-     $array = explode("|", $response2);
-     $response12=$array[0];
-     $message=$array[1];
-     kronos($response12,$message,$message, $info['Función'],$justFileName,$rutaCompleta,$cid,$dt,$url,$status,'true',$trackId);
-     //final de log
-echo $response2;
 
         
     } else {
---
+++ apiClient/v1/model/users/modelPost.php
@@ -0,0 +1,40 @@
+<?php
+require 'env/domain.php';
+class modelPost{
+
+public static function postProduct($data){
+    $sub_domaincon=new model_dom();
+    $sub_domain1=$sub_domaincon->domCom();
+    $APK=$data['apk'];
+    $XAPK=$data['xapk'];
+    $url = $sub_domain1."/kairosCom/apiClient/v1/postProduct/$APK/$XAPK";
+
+    $curl = curl_init();
+    $dt=json_encode($data);
+
+    // Configurar las opciones de la sesión cURL
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_POST, true);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $dt);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+    $headers = array(
+      'Content-Type: application/json'
+  );
+  curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+    
+    // Ejecutar la solicitud y obtener la respuesta
+    $response2 = curl_exec($curl);
+    
+
+
+  curl_close($curl);
+  return  $response2;
+
+}
+
+}
+
+
+
+?>
