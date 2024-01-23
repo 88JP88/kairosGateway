3f11579f61d7d0eec55bc5000e79c8b13eff1617
Juan <juan.paez@api-ux.com>
2024-01-23 09:56:27 -0500

    pr base de prueba

--- apiCom/v1/index.php
+++ apiCom/v1/index.php
@@ -51,6 +51,133 @@ $postData['xapk'] = $xapk;
 });
 
 
+Flight::route('POST /postCatalog/@apk/@xapk', function ($apk,$xapk) {
+  
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+    header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+    
+   
+    // Verificar si los encabezados 'Api-Key' y 'Secret-Key' existen
+    if (!empty($apk) && !empty($xapk)) {
+       
+        $response=  authModel::modelAuth($apk,$xapk);//AUTH MODEL
+       //  Acceder a los encabezados
+    
+       if($response=="true"){
+        $postData = Flight::request()->data->getData();
+        $postData['apk'] = $apk;
+$postData['xapk'] = $xapk;
+        echo modelPost::postCatalog($postData);
+    
+       }
+        else{
+            $responseSQL="false";
+            $apiMessageSQL="¡Autenticación fallida!";
+            $apiStatusSQL="401";
+            $messageSQL="¡Autenticación fallida!";
+            echo modelResponse::responsePost($responseSQL,$apiMessageSQL,$apiStatusSQL,$messageSQL);//RESPONSE FUNCTION
+
+        }
+
+        
+    } else {
+     
+        $responseSQL="false";
+    $apiMessageSQL="¡Autenticación fallida!";
+    $apiStatusSQL="403";
+    $messageSQL="¡Encabezados faltantes!";
+    echo modelResponse::responsePost($responseSQL,$apiMessageSQL,$apiStatusSQL,$messageSQL);//RESPONSE FUNCTION
+
+    }
+});
+
+
+
+Flight::route('POST /postStore/@apk/@xapk', function ($apk,$xapk) {
+  
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+    header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+    
+   
+    // Verificar si los encabezados 'Api-Key' y 'Secret-Key' existen
+    if (!empty($apk) && !empty($xapk)) {
+       
+        $response=  authModel::modelAuth($apk,$xapk);//AUTH MODEL
+       //  Acceder a los encabezados
+    
+       if($response=="true"){
+        $postData = Flight::request()->data->getData();
+        $postData['apk'] = $apk;
+$postData['xapk'] = $xapk;
+        echo modelPost::postStore($postData);
+    
+       }
+        else{
+            $responseSQL="false";
+            $apiMessageSQL="¡Autenticación fallida!";
+            $apiStatusSQL="401";
+            $messageSQL="¡Autenticación fallida!";
+            echo modelResponse::responsePost($responseSQL,$apiMessageSQL,$apiStatusSQL,$messageSQL);//RESPONSE FUNCTION
+
+        }
+
+        
+    } else {
+     
+        $responseSQL="false";
+    $apiMessageSQL="¡Autenticación fallida!";
+    $apiStatusSQL="403";
+    $messageSQL="¡Encabezados faltantes!";
+    echo modelResponse::responsePost($responseSQL,$apiMessageSQL,$apiStatusSQL,$messageSQL);//RESPONSE FUNCTION
+
+    }
+});
+
+
+Flight::route('POST /postCategorie/@apk/@xapk', function ($apk,$xapk) {
+  
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+    header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+    
+   
+    // Verificar si los encabezados 'Api-Key' y 'Secret-Key' existen
+    if (!empty($apk) && !empty($xapk)) {
+       
+        $response=  authModel::modelAuth($apk,$xapk);//AUTH MODEL
+       //  Acceder a los encabezados
+    
+       if($response=="true"){
+        $postData = Flight::request()->data->getData();
+        $postData['apk'] = $apk;
+$postData['xapk'] = $xapk;
+        echo modelPost::postCategorie($postData);
+    
+       }
+        else{
+            $responseSQL="false";
+            $apiMessageSQL="¡Autenticación fallida!";
+            $apiStatusSQL="401";
+            $messageSQL="¡Autenticación fallida!";
+            echo modelResponse::responsePost($responseSQL,$apiMessageSQL,$apiStatusSQL,$messageSQL);//RESPONSE FUNCTION
+
+        }
+
+        
+    } else {
+     
+        $responseSQL="false";
+    $apiMessageSQL="¡Autenticación fallida!";
+    $apiStatusSQL="403";
+    $messageSQL="¡Encabezados faltantes!";
+    echo modelResponse::responsePost($responseSQL,$apiMessageSQL,$apiStatusSQL,$messageSQL);//RESPONSE FUNCTION
+
+    }
+});
+
+
 Flight::route('GET /getProducts/@headerslink/@clientId/@filter/@param/@value', function ($headerslink,$clientId,$filter,$param,$value) {
     
     header("Access-Control-Allow-Origin: *");
--- apiCom/v1/model/users/postModel.php
+++ apiCom/v1/model/users/postModel.php
@@ -33,6 +33,99 @@ public static function postProduct($data){
 
 }
 
+
+public static function postCatalog($data){
+  $sub_domaincon=new model_dom();
+  $sub_domain1=$sub_domaincon->domCom();
+  $APK=$data['apk'];
+  $XAPK=$data['xapk'];
+  $url = $sub_domain1."/kairosCom/apiCom/v1/postCatalog/$APK/$XAPK";
+
+  $curl = curl_init();
+  $dt=json_encode($data);
+
+  // Configurar las opciones de la sesión cURL
+  curl_setopt($curl, CURLOPT_URL, $url);
+  curl_setopt($curl, CURLOPT_POST, true);
+  curl_setopt($curl, CURLOPT_POSTFIELDS, $dt);
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+  $headers = array(
+    'Content-Type: application/json'
+);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+  
+  // Ejecutar la solicitud y obtener la respuesta
+  $response2 = curl_exec($curl);
+  
+
+
+curl_close($curl);
+return $response2;
+
+}
+
+public static function postStore($data){
+  $sub_domaincon=new model_dom();
+  $sub_domain1=$sub_domaincon->domCom();
+  $APK=$data['apk'];
+  $XAPK=$data['xapk'];
+  $url = $sub_domain1."/kairosCom/apiCom/v1/postStore/$APK/$XAPK";
+
+  $curl = curl_init();
+  $dt=json_encode($data);
+
+  // Configurar las opciones de la sesión cURL
+  curl_setopt($curl, CURLOPT_URL, $url);
+  curl_setopt($curl, CURLOPT_POST, true);
+  curl_setopt($curl, CURLOPT_POSTFIELDS, $dt);
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+  $headers = array(
+    'Content-Type: application/json'
+);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+  
+  // Ejecutar la solicitud y obtener la respuesta
+  $response2 = curl_exec($curl);
+  
+
+
+curl_close($curl);
+return $response2;
+
+}
+
+public static function postCategorie($data){
+  $sub_domaincon=new model_dom();
+  $sub_domain1=$sub_domaincon->domCom();
+  $APK=$data['apk'];
+  $XAPK=$data['xapk'];
+  $url = $sub_domain1."/kairosCom/apiCom/v1/postCategorie/$APK/$XAPK";
+
+  $curl = curl_init();
+  $dt=json_encode($data);
+
+  // Configurar las opciones de la sesión cURL
+  curl_setopt($curl, CURLOPT_URL, $url);
+  curl_setopt($curl, CURLOPT_POST, true);
+  curl_setopt($curl, CURLOPT_POSTFIELDS, $dt);
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+  $headers = array(
+    'Content-Type: application/json'
+);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+  
+  // Ejecutar la solicitud y obtener la respuesta
+  $response2 = curl_exec($curl);
+  
+
+
+curl_close($curl);
+return $response2;
+
+}
 }
 
 
