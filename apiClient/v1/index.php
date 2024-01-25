8ead2f4aad4793db1c964938a282a0c4946b4e3e
Juan <juan.paez@api-ux.com>
2024-01-25 11:53:14 -0500

    pr base de prueba

--- apiClient/v1/index.php
+++ apiClient/v1/index.php
@@ -473,6 +473,7 @@ Flight::route('GET /getClientOrders/@headerslink/@apiData', function ($headersli
     header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
     header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+   
     $decodedData = urldecode($apiData);
     $postData = json_decode($decodedData, true);
     $parts = explode(" ", $headerslink);
