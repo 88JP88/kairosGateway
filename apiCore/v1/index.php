316089b6344e989ccd3e7e1f4773f0ec7a557f30
paezgit88 <120344753+paezgit88@users.noreply.github.com>
2024-02-14 13:16:46 -0500

    A

--- apiCore/v1/index.php
+++ apiCore/v1/index.php
@@ -512,8 +512,8 @@ Flight::route('GET /getGeneralUsers/@headerslink/@apiData', function ($headersli
                 "endPoint"=>"getGeneralUsers"
             ];
            
-                    //echo modelGet::getModel($postData);      
-                    echo $apiData;     
+                    echo modelGet::getModel($postData);      
+                    //echo $apiData;     
         } else {
             $responseSQL="false";
             $apiMessageSQL="¡Autenticación fallida!";
