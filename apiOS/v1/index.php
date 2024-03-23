142308305f7b3a566b4cefbd51d4be8bb4a671af
paezgit88 <120344753+paezgit88@users.noreply.github.com>
2024-03-23 18:09:27 -0500

    A

--- apiOS/v1/index.php
+++ apiOS/v1/index.php
@@ -481,7 +481,7 @@ $postData['apiValues'] = [
 
 
 
-Flight::route('POST /putCatalogBulk/@apk/@xapk', function ($apk,$xapk) {
+Flight::route('POST /postProduct/@apk/@xapk', function ($apk,$xapk) {
   
     header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
@@ -490,477 +490,43 @@ Flight::route('POST /putCatalogBulk/@apk/@xapk', function ($apk,$xapk) {
    
     // Verificar si los encabezados 'Api-Key' y 'Secret-Key' existen
     if (!empty($apk) && !empty($xapk)) {
-        $dta = array(
-            
-            'bulk' => Flight::request()->data->bulk,
-            'clientId' => Flight::request()->data->clientId
-
-        );
-
-
-
-        // Acceder a los encabezados
+       
+        $response=  authModel::modelAuth($apk,$xapk);//AUTH MODEL
+       //  Acceder a los encabezados
     
-        
+       if($response=="true"){
+        $postData = Flight::request()->data->getData();
+        $postData['apk'] = $apk;
+$postData['xapk'] = $xapk;
+$postData['apiValues'] = [
+    "serviceName"=>"kairosOS",
+    "apiName"=>"apiOS",
+    "apiVersion"=>"v1",
+    "endPoint"=>"postElement"
+];
+        echo modelPost::postModel($postData);
+    
+       }
+        else{
+            $responseSQL="false";
+            $apiMessageSQL="¡Autenticación fallida!";
+            $apiStatusSQL="401";
+            $messageSQL="¡Autenticación fallida!";
+            echo modelResponse::responsePost($responseSQL,$apiMessageSQL,$apiStatusSQL,$messageSQL);//RESPONSE FUNCTION
 
-        $sub_domaincon=new model_dom();
-        $sub_domain=$sub_domaincon->dom();
-        $url = $sub_domain.'/kairosCore/apiAuth/v1/authApiKeyGateway/';
-      
-        $data = array(
-          'ApiKey' =>$apk, 
-          'xapiKey' => $xapk
-          
-          );
-      $curl = curl_init();
-      
-      // Configurar las opciones de la sesión cURL
-      curl_setopt($curl, CURLOPT_URL, $url);
-      curl_setopt($curl, CURLOPT_POST, true);
-      curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-      curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-      // curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-      
-      // Ejecutar la solicitud y obtener la respuesta
-      $response1 = curl_exec($curl);
-
-      
-      $dt=json_encode($dta);
-
-      curl_close($curl);
-      $sub_domain1=$sub_domaincon->domCom();
-      $url = $sub_domain1."/kairosCom/apiClient/v1/putCatalogBulk/$apk/$xapk";
-
-      $curl = curl_init();
-      
-      // Configurar las opciones de la sesión cURL
-      curl_setopt($curl, CURLOPT_URL, $url);
-      curl_setopt($curl, CURLOPT_POST, true);
-      curl_setopt($curl, CURLOPT_POSTFIELDS, $dt);
-      curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-
-      $headers = array(
-        'Content-Type: application/json'
-    );
-    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-      
-      // Ejecutar la solicitud y obtener la respuesta
-      $response2 = curl_exec($curl);
-      
-
- 
-    curl_close($curl);
-
-echo $response2;
+        }
 
         
     } else {
-        echo 'false|¡Error: Encabezados faltantes!';
+     
+        $responseSQL="false";
+    $apiMessageSQL="¡Autenticación fallida!";
+    $apiStatusSQL="403";
+    $messageSQL="¡Encabezados faltantes!";
+    echo modelResponse::responsePost($responseSQL,$apiMessageSQL,$apiStatusSQL,$messageSQL);//RESPONSE FUNCTION
+
     }
 });
-
-
-
-
-
-
-
-Flight::route('POST /putProductBulk/@apk/@xapk', function ($apk,$xapk) {
-  
-    header("Access-Control-Allow-Origin: *");
-    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-    header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
-    
-   
-    // Verificar si los encabezados 'Api-Key' y 'Secret-Key' existen
-    if (!empty($apk) && !empty($xapk)) {
-        $dta = array(
-            
-            'bulk' => Flight::request()->data->bulk,
-            'clientId' => Flight::request()->data->clientId
-
-        );
-
-
-
-        // Acceder a los encabezados
-    
-        
-
-        $sub_domaincon=new model_dom();
-        $sub_domain=$sub_domaincon->dom();
-        $url = $sub_domain.'/kairosCore/apiAuth/v1/authApiKeyGateway/';
-      
-        $data = array(
-          'ApiKey' =>$apk, 
-          'xapiKey' => $xapk
-          
-          );
-      $curl = curl_init();
-      
-      // Configurar las opciones de la sesión cURL
-      curl_setopt($curl, CURLOPT_URL, $url);
-      curl_setopt($curl, CURLOPT_POST, true);
-      curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-      curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-      // curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-      
-      // Ejecutar la solicitud y obtener la respuesta
-      $response1 = curl_exec($curl);
-
-      
-      $dt=json_encode($dta);
-
-      curl_close($curl);
-      $sub_domain1=$sub_domaincon->domCom();
-      $url = $sub_domain1."/kairosCom/apiClient/v1/putProductBulk/$apk/$xapk";
-
-      $curl = curl_init();
-      
-      // Configurar las opciones de la sesión cURL
-      curl_setopt($curl, CURLOPT_URL, $url);
-      curl_setopt($curl, CURLOPT_POST, true);
-      curl_setopt($curl, CURLOPT_POSTFIELDS, $dt);
-      curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-
-      $headers = array(
-        'Content-Type: application/json'
-    );
-    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-      
-      // Ejecutar la solicitud y obtener la respuesta
-      $response2 = curl_exec($curl);
-      
-
- 
-    curl_close($curl);
-
-echo $response2;
-
-        
-    } else {
-        echo 'false|¡Error: Encabezados faltantes!';
-    }
-});
-
-
-Flight::route('POST /postStoreBulk/@apk/@xapk', function ($apk,$xapk) {
-  
-    header("Access-Control-Allow-Origin: *");
-    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-    header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
-    
-   
-    // Verificar si los encabezados 'Api-Key' y 'Secret-Key' existen
-    if (!empty($apk) && !empty($xapk)) {
-        $dta = array(
-            
-            'bulk' => Flight::request()->data->bulk,
-            'clientId' => Flight::request()->data->clientId
-
-        );
-
-
-
-        // Acceder a los encabezados
-    
-        
-
-        $sub_domaincon=new model_dom();
-        $sub_domain=$sub_domaincon->dom();
-        $url = $sub_domain.'/kairosCore/apiAuth/v1/authApiKeyGateway/';
-      
-        $data = array(
-          'ApiKey' =>$apk, 
-          'xapiKey' => $xapk
-          
-          );
-      $curl = curl_init();
-      
-      // Configurar las opciones de la sesión cURL
-      curl_setopt($curl, CURLOPT_URL, $url);
-      curl_setopt($curl, CURLOPT_POST, true);
-      curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-      curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-      // curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-      
-      // Ejecutar la solicitud y obtener la respuesta
-      $response1 = curl_exec($curl);
-
-      
-      $dt=json_encode($dta);
-
-      curl_close($curl);
-      $sub_domain1=$sub_domaincon->domCom();
-      $url = $sub_domain1."/kairosCom/apiClient/v1/postStoreBulk/$apk/$xapk";
-
-      $curl = curl_init();
-      
-      // Configurar las opciones de la sesión cURL
-      curl_setopt($curl, CURLOPT_URL, $url);
-      curl_setopt($curl, CURLOPT_POST, true);
-      curl_setopt($curl, CURLOPT_POSTFIELDS, $dt);
-      curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-
-      $headers = array(
-        'Content-Type: application/json'
-    );
-    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-      
-      // Ejecutar la solicitud y obtener la respuesta
-      $response2 = curl_exec($curl);
-      
-
- 
-    curl_close($curl);
-
-echo $response2;
-
-        
-    } else {
-        echo 'false|¡Error: Encabezados faltantes!';
-    }
-});
-
-
-Flight::route('POST /putStoreBulk/@apk/@xapk', function ($apk,$xapk) {
-  
-    header("Access-Control-Allow-Origin: *");
-    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-    header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
-    
-   
-    // Verificar si los encabezados 'Api-Key' y 'Secret-Key' existen
-    if (!empty($apk) && !empty($xapk)) {
-        $dta = array(
-            
-            'bulk' => Flight::request()->data->bulk,
-            'clientId' => Flight::request()->data->clientId
-
-        );
-
-
-
-        // Acceder a los encabezados
-    
-        
-
-        $sub_domaincon=new model_dom();
-        $sub_domain=$sub_domaincon->dom();
-        $url = $sub_domain.'/kairosCore/apiAuth/v1/authApiKeyGateway/';
-      
-        $data = array(
-          'ApiKey' =>$apk, 
-          'xapiKey' => $xapk
-          
-          );
-      $curl = curl_init();
-      
-      // Configurar las opciones de la sesión cURL
-      curl_setopt($curl, CURLOPT_URL, $url);
-      curl_setopt($curl, CURLOPT_POST, true);
-      curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-      curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-      // curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-      
-      // Ejecutar la solicitud y obtener la respuesta
-      $response1 = curl_exec($curl);
-
-      
-      $dt=json_encode($dta);
-
-      curl_close($curl);
-      $sub_domain1=$sub_domaincon->domCom();
-      $url = $sub_domain1."/kairosCom/apiClient/v1/putStoreBulk/$apk/$xapk";
-
-      $curl = curl_init();
-      
-      // Configurar las opciones de la sesión cURL
-      curl_setopt($curl, CURLOPT_URL, $url);
-      curl_setopt($curl, CURLOPT_POST, true);
-      curl_setopt($curl, CURLOPT_POSTFIELDS, $dt);
-      curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-
-      $headers = array(
-        'Content-Type: application/json'
-    );
-    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-      
-      // Ejecutar la solicitud y obtener la respuesta
-      $response2 = curl_exec($curl);
-      
-
- 
-    curl_close($curl);
-
-echo $response2;
-
-        
-    } else {
-        echo 'false|¡Error: Encabezados faltantes!';
-    }
-});
-
-
-
-Flight::route('POST /postCategorieBulk/@apk/@xapk', function ($apk,$xapk) {
-  
-    header("Access-Control-Allow-Origin: *");
-    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-    header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
-    
-   
-    // Verificar si los encabezados 'Api-Key' y 'Secret-Key' existen
-    if (!empty($apk) && !empty($xapk)) {
-        $dta = array(
-            
-            'bulk' => Flight::request()->data->bulk,
-            'clientId' => Flight::request()->data->clientId
-
-        );
-
-
-
-        // Acceder a los encabezados
-    
-        
-
-        $sub_domaincon=new model_dom();
-        $sub_domain=$sub_domaincon->dom();
-        $url = $sub_domain.'/kairosCore/apiAuth/v1/authApiKeyGateway/';
-      
-        $data = array(
-          'ApiKey' =>$apk, 
-          'xapiKey' => $xapk
-          
-          );
-      $curl = curl_init();
-      
-      // Configurar las opciones de la sesión cURL
-      curl_setopt($curl, CURLOPT_URL, $url);
-      curl_setopt($curl, CURLOPT_POST, true);
-      curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-      curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-      // curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-      
-      // Ejecutar la solicitud y obtener la respuesta
-      $response1 = curl_exec($curl);
-
-      
-      $dt=json_encode($dta);
-
-      curl_close($curl);
-      $sub_domain1=$sub_domaincon->domCom();
-      $url = $sub_domain1."/kairosCom/apiClient/v1/postCategorieBulk/$apk/$xapk";
-
-      $curl = curl_init();
-      
-      // Configurar las opciones de la sesión cURL
-      curl_setopt($curl, CURLOPT_URL, $url);
-      curl_setopt($curl, CURLOPT_POST, true);
-      curl_setopt($curl, CURLOPT_POSTFIELDS, $dt);
-      curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-
-      $headers = array(
-        'Content-Type: application/json'
-    );
-    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-      
-      // Ejecutar la solicitud y obtener la respuesta
-      $response2 = curl_exec($curl);
-      
-
- 
-    curl_close($curl);
-
-echo $response2;
-
-        
-    } else {
-        echo 'false|¡Error: Encabezados faltantes!';
-    }
-});
-
-
-
-Flight::route('POST /putCategorieBulk/@apk/@xapk', function ($apk,$xapk) {
-  
-    header("Access-Control-Allow-Origin: *");
-    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-    header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
-    
-   
-    // Verificar si los encabezados 'Api-Key' y 'Secret-Key' existen
-    if (!empty($apk) && !empty($xapk)) {
-        $dta = array(
-            
-            'bulk' => Flight::request()->data->bulk,
-            'clientId' => Flight::request()->data->clientId
-
-        );
-
-
-
-        // Acceder a los encabezados
-    
-        
-
-        $sub_domaincon=new model_dom();
-        $sub_domain=$sub_domaincon->dom();
-        $url = $sub_domain.'/kairosCore/apiAuth/v1/authApiKeyGateway/';
-      
-        $data = array(
-          'ApiKey' =>$apk, 
-          'xapiKey' => $xapk
-          
-          );
-      $curl = curl_init();
-      
-      // Configurar las opciones de la sesión cURL
-      curl_setopt($curl, CURLOPT_URL, $url);
-      curl_setopt($curl, CURLOPT_POST, true);
-      curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-      curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-      // curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-      
-      // Ejecutar la solicitud y obtener la respuesta
-      $response1 = curl_exec($curl);
-
-      
-      $dt=json_encode($dta);
-
-      curl_close($curl);
-      $sub_domain1=$sub_domaincon->domCom();
-      $url = $sub_domain1."/kairosCom/apiClient/v1/putCategorieBulk/$apk/$xapk";
-
-      $curl = curl_init();
-      
-      // Configurar las opciones de la sesión cURL
-      curl_setopt($curl, CURLOPT_URL, $url);
-      curl_setopt($curl, CURLOPT_POST, true);
-      curl_setopt($curl, CURLOPT_POSTFIELDS, $dt);
-      curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-
-      $headers = array(
-        'Content-Type: application/json'
-    );
-    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-      
-      // Ejecutar la solicitud y obtener la respuesta
-      $response2 = curl_exec($curl);
-      
-
- 
-    curl_close($curl);
-
-echo $response2;
-
-        
-    } else {
-        echo 'false|¡Error: Encabezados faltantes!';
-    }
-});
-
 
 
 
